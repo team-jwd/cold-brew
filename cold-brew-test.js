aa8f18175505617bf953601d300ae381f2c9d2ad
William Barnes <barnesw91291@gmail.com>
2016-10-31 16:03:23 -0700

    maybe?

--- cold-brew-test.js
+++ cold-brew-test.js
@@ -31,6 +31,7 @@ function createClient() {
   //     },
   //   })
   //   .build();
+  
   if (process.env.SAUCE_USERNAME !== undefined) {
     client = new selenium.Builder()
       .usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
